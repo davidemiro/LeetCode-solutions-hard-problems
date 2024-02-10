d01b0b44925a47cecb6c95e03eac17f473473618
Davide Miro <44341307+davidemiro@users.noreply.github.com>
2024-02-10 23:43:38 +0100

    Time: 17 ms (45.62%) | Memory: 19.2 MB (47.29%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <set>
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sol =nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            sol = sol ^ nums[i];
+        }
+        return sol;
+        
+    }
+};
